5371bf2af782f9664914480250d9909441d93317
ly66857 <2425778601@qq.com>
2020-05-26 21:18:37 +0800

    二次提交

--- src/tlProjec/Test.java
+++ src/tlProjec/Test.java
@@ -2,7 +2,7 @@ package tlProjec;
 
 public class Test {
 public static void main(String[] args) {
-	System.out.println("²âÊÔ001");
-	System.out.println("其他人更改);
+	System.out.println("第一次编码");
+	System.out.println("第二次");
 }
 }
